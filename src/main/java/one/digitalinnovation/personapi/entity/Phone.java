82a9f31eb751d1ec0ef05d3921ba79f5e981c33d
Lord Vinick <vinny.queiyros16@gmail.com>
2022-02-27 20:07:45 -0300

    Commi 2

---
+++ src/main/java/one/digitalinnovation/personapi/entity/Phone.java
@@ -0,0 +1,33 @@
+package one.digitalinnovation.personapi.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.*;
+import one.digitalinnovation.personapi.enums.PhoneType;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Getter
+public class Phone {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Enumerated(EnumType.STRING)
+	@Column(nullable = false)
+	private PhoneType type;
+	
+	@Column(nullable = false)
+	private String number;
+
+}
